2325dec9fa3f03e1560c412e4b8dbccedfefc81d
J Combes <jcombes9834@gmail.com>
2021-09-27 15:49:55 -0400

    2020 analysis no longer supported - included only for reference. Update function names to be compatible with push towards object oriented programming. Started work on class files

---
+++ airplane.m
@@ -0,0 +1,16 @@
+classdef airplane
+	
+	properties
+		wing
+		empennage
+		powerSystem
+		fuselage
+	end
+	
+	methods
+		%function obj = airplane(wing, empennage, powerSystem, fuselage)
+
+		%end
+	end	
+
+end
