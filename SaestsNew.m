0e3a67fd8a4e15313dc443d74886ef4877fde601
akaash4 <mr.awsome002@gmail.com>
2021-10-23 15:04:02 -0400

    Shortened Fuselage code
    Should be good to go for final analysis, has comments everywhere

--- SaestsNew.m
+++ SaestsNew.m
@@ -5,177 +5,52 @@ function [FSA,fh,fw,fl,Weightfuse,SAfT] = SaestsNew(ns) %surface area estimate f
 
 %Akaash: How I'm thinking about putting the syringes is in groups of five
 %on the fuselage and then just stacking these groups of five on top of each other
-
-sw=1.25; %width syringe (in)
-sh=1.75; %height syringe (in)
-sl=6; %length syringe (in)
+sw=1.5; %width syringe (in)
+sh=2; %height syringe (in)
+sl=6.25; %length syringe (in)
 nc=0; %number of crates
 cl=3.5; %crate length (in) 
 ch=cl; %crate height
 rml=5; %release mechanism length (in)
+sp=1.25; %spacer length between each crate 
+%DEBUG rmar is just a ratio to find fuselage height, can change, also can change the release mechanism length based on what we pick 
 rmar=2; %release mechanism aspect ratio <---- Just made it 2 for now, can change later
 fh=0; %fuse height (in)
 fw=0; %fuse width (in)
 fl=0; %fuse length (in)
 rhoCarbon=120.486; %lb/ft^3
+%DEBUG can change spr to what we want it to be
+spr=5; %syringes per row depending on how many we want 
 
-if ns<=5 %this defines the function for the number of syringes we will carry 
-    nc=0; %can only carry 1 crate if we carry 10 syringes
-    rml=nc*cl+rml; %this will just be the length of the release mechanism so that we take it into account since we are dropping crates out the back of the fuselage
-    fh=ch*rmar; %this will just be crate height x a ratio we want to ensure they are easily able to fit and also take into account mechanim height. 
-    fw=sw*5; %I'm thinking of laying out the syringes in rows of five, so this is why this is sw*5
-    if sh<fh %this if will be used to extend the length of the fuselage accordingly depending on how many syringes we will carry
-        fl=rml+sl; 
-    elseif sh>=fh %if the syringe height after stacking them exceeds the fuselage height, we have to extend the fueslage so we have more space to put them
-        a = sh/fh;
-        a = ceil(a); %this a variable will just 
-        fl=a*sl+rml;
-    end
-elseif ns<=10
-    nc=1;
-    rml=nc*cl+rml;  
-    fh=ch*rmar; 
-    fw=sw*5; 
-    sh=2*sh;
-    if sh<fh
-        fl=rml+sl; 
-    elseif sh>=fh
-        a = sh/fh;
-        a = ceil(a);
-        fl=a*sl+rml;
-    end
-elseif ns<=15
-    nc=1;
-    rml=nc*cl+rml;  
-    fh=ch*rmar; 
-    fw=sw*5; 
-    sh=3*sh;
-    if sh<fh
-        fl=rml+sl; 
-    elseif sh>=fh
-        a = sh/fh;
-        a = ceil(a);
-        fl=a*sl+rml;
-    end
-elseif ns<=20
-    nc=2;
-    rml=nc*cl+rml+1.25;  
-    fh=ch*rmar; 
-    fw=sw*5; 
-    sh=4*sh;
-    if sh<fh
-        fl=rml+sl; 
-    elseif sh>=fh
-        a = sh/fh;
-        a = ceil(a);
-        fl=a*sl+rml;
-    end
-elseif ns<=25
-    nc=2;
-    rml=nc*cl+rml+1.25;  
-    fh=ch*rmar; 
-    fw=sw*5; 
-    sh=5*sh;
-    if sh<fh
-        fl=rml+sl; 
-    elseif sh>=fh
-        a = sh/fh;
-        a = ceil(a);
-        fl=a*sl+rml;
-    end
-elseif ns<=30
-    nc=3;
-    rml=nc*cl+rml+2.5;  
-    fh=ch*rmar; 
-    fw=sw*5; 
-    sh=6*sh;
-    if sh<fh
-        fl=rml+sl; 
-    elseif sh>=fh
-        a = sh/fh;
-        a = ceil(a);
-        fl=a*sl+rml;
-    end
-elseif ns<=35
-    nc=3;
-    rml=nc*cl+rml+2.5;  
-    fh=ch*rmar; 
-    fw=sw*5; 
-    sh=7*sh;
-    if sh<fh
-        fl=rml+sl; 
-    elseif sh>=fh
-        a = sh/fh;
-        a = ceil(a);
-        fl=a*sl+rml;
-    end
-elseif ns<=40
-    nc=4;
-    rml=nc*cl+rml+3.75;  
-    fh=ch*rmar; 
-    fw=sw*5; 
-    sh=8*sh;
-    if sh<fh
-        fl=rml+sl; 
-    elseif sh>=fh
-        a = sh/fh;
-        a = ceil(a);
-        fl=a*sl+rml;
-    end
-elseif ns<=45
-    nc=4;
-    rml=nc*cl+rml+3.75;  
-    fh=ch*rmar; 
-    fw=sw*5; 
-    sh=9*sh;
-    if sh<fh
-        fl=rml+sl; 
-    elseif sh>=fh
-        a = sh/fh;
-        a = ceil(a);
-        fl=a*sl+rml;
-    end
-elseif ns<=50
-    nc=5;
-    rml=nc*cl+rml+5;  
-    fh=ch*rmar; 
-    fw=sw*5; 
-    sh=10*sh;
-    if sh<fh
-        fl=rml+sl; 
-    elseif sh>=fh
-        a = sh/fh;
-        a = ceil(a);
-        fl=a*sl+rml;
-    end
-elseif ns<=55
-    nc=5;
-    rml=nc*cl+rml+5;  
-    fh=ch*rmar; 
-    fw=sw*5; 
-    sh=11*sh;
-    if sh<fh
-        fl=rml+sl; 
-    elseif sh>=fh
-        a = sh/fh;
-        a = ceil(a);
-        fl=a*sl+rml;
-    end
+fh=ch*rmar; %this will just be crate height x a ratio we want to ensure they are easily able to fit and also take into account mechanim height. 
+fw=sw*spr; %I'm thinking of laying out the syringes in rows of five, so this is why this is sw*5
+nc=floor(ns/10); %this will calculate number of crates we can carry
+tsh=sh*(ceil(ns/spr)); %this will calculate the total syringe height if we lay them out in rows of 5
+frml=nc*cl+((nc-1)*sp)+rml; %this will just be the final length of the release mechanism so that we take it into account since we are dropping crates out the back of the fuselage
+
+%this is used to find the length of the fuselage
+if tsh<fh %this if will be used to extend the length of the fuselage accordingly depending on how many syringes we will carry
+    ttfl=frml+sl; %temporary temporary fueslage length
+elseif tsh>=fh %if the syringe height after stacking them exceeds the fuselage height, we have to extend the fueslage so we have more space to put them
+    a = tsh/fh;
+    a = ceil(a); %this a variable will just 
+    ttfl=a*sl+frml; %temporary temporary fuselage length
 end
 
-%Akaash: I still do not understand these equations that were made last year
-%and do not know if I should make new functions or not because I am not
-%sure if these functions account for the extra space that will be needed
-%in the fuselage for systems things. Honestly just need clarification on
-%what these equations are really trying to say
-SAbrick=2*fl*fh+2*fl*fw;
-Hf=sqrt( (0.5*fh)^2 +6^2);
-SAfTri=2*Hf*fw + fh*6;
-Hb=sqrt( fh^2 +8^2);
-SAbTri=fw*8 + fw*Hb + fh*8;
-SAfT=SAbrick+SAfTri+ SAbTri;
+sys=8; %space systems needs for their stuff
+tfl=ttfl+sys; %add systems length for avionics package, and this will be the temporary length used only for the main part of the fuselage
+fla=6; %assumption for the front length of the fuselage
+bla=6; %assumption for the back length of the fueslage
+fl=tfl+fla+bla; %final length of fuselage
+
+SAm=(2*tfl*fh)+(2*tfl*fw); %this will be the surface area of the main portion of the fuselage, aka the middle part
+Df=sqrt((0.5*fh)^2+(fla)^2); %we will assume a trapezoidal shape for the front of the fuselage if you view it from the side, so df will be the diagonal length.  
+SAf=(Df*fw)+(6*fw)+((fh/2)*fw)+2*((0.75*fh*6)); %this formula is used to the find the surface area of the front section. So it is bottom area + top area + front area(closed section which is the nose) + side area, which are trapezoids
+Db=sqrt((fh)^2+(bla)^2); %we will assume a triangular shape for the back of the fuselage if you view it from the side since we need a ramp to release the boxes from. 
+SAb=(fw*bla)+(fw*Db)+2*((fh*bla)/2); %this formula is used to find the surface area of the back portion of the fueslage. Same method as front except now we have triangular surface area instead of trapezoidal.
+SAfT=SAm+SAf+SAb; %this will be the surface area of the fuselage total in inches
 SAfT=SAfT/144;%Surface Area fuse Total,ft^2
-FSA=fw*fh/144;%Frontal Surface Area,ft^2
-Vol=SAfT*(1/16)/12;%The volume in ft^3
-Weightfuse=Vol *rhoCarbon;
+FSA=(fw*fh)/144;%Frontal Surface Area,ft^2
+Vol=SAfT*((1/16)/12);%The volume in ft^3, 1/16 is the thickness of the foam in inches so this will help us find the weight of the fuselage
+Weightfuse=Vol *rhoCarbon; %Weight in lb
 end
