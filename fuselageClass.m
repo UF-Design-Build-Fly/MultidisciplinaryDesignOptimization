ae09bf26bba05624bd6af10c15b9f75aa43add8e
J Combes <jcombes9834@gmail.com>
2021-10-23 16:42:06 -0400

    Updating work

--- fuselageClass.m
+++ fuselageClass.m
@@ -5,7 +5,7 @@ classdef fuselageClass
     frontalSurfaceArea = -1;
     height = -1;
     width = -1;
-    length = -1;
+    length = -1; %feet
     totalSA = -1;
     
     weight = -1; %weight of carbon fiber to make fuselage + mechanisms
@@ -13,7 +13,10 @@ classdef fuselageClass
     gearWeight = -1;
     gearParaDrag = -1;
     gearSA = -1;
-    wheelSA = -1;
+
+    wheelWidth = 0.5;    %width of wheels (in)
+    wheelRadius = 1.5; %wheel radius
+    wheelSA = 2*pi*(radius_wheel)^2+ pi*2*radius_wheel*width_wheel)/144;
 
     fuseDrag1 = -1;
     fuseDrag2 = -1;
--- sanityCheck.m
+++ sanityCheck.m
@@ -1,5 +1,16 @@
 function [check] = sanityCheck(plane)
 %returns true if the aircraft makes sense and meets requirements
-
-    if plane.performanceClass.takeoffDist2
+    %lap distance is ~2160 feet
+    %mission 1 velocity must be sufficient to complete 3 laps in 5 minutes
+    %i.e, minimum velocity of 21.6 feet per second
+    %use the mission two velocity for this check to save calls to velocity solver
+    %
+    
+    if (plane.performance.takeoffDist2 > 25) || (plane.performance.takeoffDist1 > 25)
+        sanityCheck = false;
+    end
+    
+    if (plane.fuselage.length > 7.9)
+        sanityCheck = false;
+    end
 end
