83b06a5a8b9e7e189e9ee7f641f89ad51624df7d
jcombes9834 <jcombes9834@gmail.com>
2022-02-05 18:42:04 -0500

    fixed dimensioning to account for correct number of vials, reduced extra cargo volume

--- fuselage.m
+++ fuselage.m
@@ -12,9 +12,9 @@ nc=0; %number of crates
 cl=3.5; %crate length (in) 
 ch=cl; %crate height
 rml=5; %release mechanism length (in)
-sp=1.25; %spacer length between each crate 
+sp=1; %spacer length between each crate 
 %DEBUG rmar is just a ratio to find fuselage height, can change, also can change the release mechanism length based on what we pick 
-rmar=2; %release mechanism aspect ratio <---- Just made it 2 for now, can change later
+rmar=1.7; %release mechanism aspect ratio (how tall compared to crate)
 fh=0; %fuse height (in)
 fw=0; %fuse width (in)
 fl=0; %fuse length (in)
@@ -26,18 +26,18 @@ spr=5; %syringes per row depending on how many we want
 
 fh=ch*rmar; %this will just be crate height x a ratio we want to ensure they are easily able to fit and also take into account mechanim height. 
 fw=sw*spr; %I'm thinking of laying out the syringes in rows of five, so this is why this is sw*5
-nc=floor(ns/10); %this will calculate number of crates we can carry
+nc= plane.fuselage.numVials;
 tsh=sh*(ceil(ns/spr)); %this will calculate the total syringe height if we lay them out in rows of 5
 frml=nc*cl+((nc-1)*sp)+rml; %this will just be the final length of the release mechanism so that we take it into account since we are dropping crates out the back of the fuselage
-cfl=frml+sl; %crate fueslage length, adding extra syringe length to account for release mechanism taking up too much space
+cfl=frml;%+sl; %crate fueslage length, adding extra syringe length to account for release mechanism taking up too much space
 
 %this is used to find the length of the fuselage
 if tsh<fh %this if will be used to extend the length of the fuselage accordingly depending on how many syringes we will carry
-    sfl=sl+rml+sl; %syringe fueslage length, adding extra syringe length to account for release mechanism taking up too much space
+    sfl=sl+rml;%+sl; %syringe fueslage length, adding extra syringe length to account for release mechanism taking up too much space
 elseif tsh>=fh %if the syringe height after stacking them exceeds the fuselage height, we have to extend the fueslage so we have more space to put them
     a=tsh/fh;
     a=ceil(a); %this a variable will just 
-    sfl=(a*sl)+rml+sl; %syringe fuselage length, adding extra syringe length to account for release mechanism taking up too much space
+    sfl=(a*sl)+rml;%;+sl; %syringe fuselage length, adding extra syringe length to account for release mechanism taking up too much space
 end
 
 %to find temporary temporary length of fuselage since we do not need to carry crates and syringes at the same time
@@ -49,8 +49,8 @@ end
 
 sys=8; %space systems needs for their stuff
 tfl=ttfl+sys; %add systems length for avionics package, and this will be the temporary length used only for the main part of the fuselage
-fla=6; %assumption for the front length of the fuselage
-bla=6; %assumption for the back length of the fueslage
+fla=3; %assumption for the front length of the fuselage.
+bla=3; %assumption for the back length of the fueslage
 fl=tfl+fla+bla; %final length of fuselage
 
 SAm=(2*tfl*fh)+(2*tfl*fw); %this will be the surface area of the main portion of the fuselage, aka the middle part
