c8683a1d1f03a89a464812aaecbee7aec88eca3b
jcombes9834 <jcombes9834@gmail.com>
2022-02-05 18:43:28 -0500

    Added weight factors for semi-empirical model

--- findTotalWeight.m
+++ findTotalWeight.m
@@ -4,7 +4,11 @@ function [plane] = findTotalWeight(plane)
 %DEBUG - Previous runs of analysis seemed to 
 %underestimate fuselage weight so just add a constant multiple here. 
 %Look into fixing root cause.
-    plane.fuselage.weight = plane.fuselage.weight * 3; 
+    plane.fuselage.weight = plane.fuselage.weight*3; 
+    plane.empennage.HSweight = plane.empennage.HSweight*1.3;
+    plane.fuselage.gearWeight = plane.fuselage.gearWeight*4;
+    plane.wing.weight = plane.wing.weight*1.15;
+    
     plane.performance.totalEmptyWeight = plane.empennage.HSweight + ...
         plane.empennage.VSweight + plane.fuselage.weight + plane.fuselage.gearWeight + ...
         plane.power.weight + plane.wing.weight;
