1b67cde09ac3a22c02f1e1aed46c59a4bf24fbea
jcombes9834 <jcombes9834@gmail.com>
2022-02-05 18:43:05 -0500

    Reviewed errors and added debugInfo - still need to check flight time failure mode

--- sanityCheck.m
+++ sanityCheck.m
@@ -4,18 +4,29 @@ function [plane] = sanityCheck(plane)
     %can only set it false. No function should set it back to true - if one
     %function thinks the plane shouldn't work then the flag should stay
     %false.
-    if ((plane.performance.takeoffDist1 > 30) || (plane.performance.takeoffDist2 > 30) || (plane.performance.takeoffDist3 > 30))
+    plane.failureReason = "OK"; %plane will stay ok if it passes all the checks below
+    if ((plane.performance.takeoffDist1 > 25) || (plane.performance.takeoffDist2 > 25) || (plane.performance.takeoffDist3 > 25))
         plane.sanityFlag = false;
+        plane.failureReason = "TakeoffDist";
+        plane.takeoffFail = 1;
     end
     if (plane.fuselage.length > 7.8)
          plane.sanityFlag = false;
+         plane.failureReason = "fuselage";
+         plane.fuselageFail = 1;
     end
-    if plane.performance.velocity2 == -1 || plane.performance.velocity3  == -1
-        disp("Error Mission 2 or 3 velocity not set"); %DEBUG - change to disp
+    if ((plane.performance.velocity2 == -1) || (plane.performance.velocity3  == -1))
+        %disp("Error Mission 2 or 3 velocity solver did not converge"); %DEBUG - change to disp instead of err.
+        plane.failureReason = "noConverge";
         plane.sanityFlag = false;
+        plane.convergeFail = 1;
     end
-    if(plane.performance.time2 < plane.power.time) %must complete 3 laps in 5 minutes
+    
+%  FIX THIS CODE LATER   
+    if( (plane.performance.time2/60) > plane.power.time) %Must have enough power to complete mission 2. 
         plane.sanityFlag = false; %for mission 3 not having enough power time will not cause a mission failure, but it is a hard requirement here
+        plane.failureReason = "Power";
+        plane.powerFail = 1;
     end
     
     %TODO: this should eventually check for -1 values in properties to make
